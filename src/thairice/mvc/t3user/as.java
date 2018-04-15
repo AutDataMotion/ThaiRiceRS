5c1b99be3f76af01352c75bd215c6b4ea95c3391
zhongweng <zhongweng.hao@qq.com>
2018-04-15 11:10:44 +0800

    test2-zw

--- src/thairice/mvc/t3user/as.java
+++ src/thairice/mvc/t3user/as.java
@@ -1,14 +1,14 @@
 package thairice.mvc.t3user;
 
-import java.util.Random;
-
 public class as {
 
 	public static void main(String[] args) {
-		//long a=System.currentTimeMillis() + (3600 * 1000);;
-		//System.out.println(new Random().nextInt(1589745));
-        System.out.println(TimeUtil.dateDiff("2018-03-05 10:09:00", "2018-02-04 14:10:23", "yyyy-MM-dd HH:mm:ss", "d")<=0);
-		//System.out.println(TimeUtil.getHour());
+		// long a=System.currentTimeMillis() + (3600 * 1000);;
+		// System.out.println(new Random().nextInt(1589745));
+		System.out.println(
+				TimeUtil.dateDiff("2018-03-05 10:09:00", "2018-02-04 14:10:23", "yyyy-MM-dd HH:mm:ss", "d") <= 0);
+		// System.out.println(TimeUtil.getHour());
+		// test by zw
 	}
 
 }
