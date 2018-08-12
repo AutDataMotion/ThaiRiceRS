d235a2e324fbe964cf9ac10dfde02c3a895a09a6
zhongweng <zhongweng.hao@qq.com>
2018-08-12 12:56:30 +0800

    M+ job 逻辑done

--- src/thairice/rpcjob/GrouthMonitorScheduleJob.java
+++ src/thairice/rpcjob/GrouthMonitorScheduleJob.java
@@ -5,13 +5,15 @@
  */
 package thairice.rpcjob;
 
-import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.CollectionUtils;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.jfinal.log.Logger;
 import com.jfinal.plugin.activerecord.Record;
 import com.jfinal.plugin.cron4j.ITask;
@@ -24,6 +26,7 @@ import csuduc.platform.util.tuple.TupleUtil;
 import thairice.config.ConfMain;
 import thairice.constant.EnumStatus;
 import thairice.mvc.t12preprocessinf.T12PreProcessInf;
+import thairice.mvc.t2syslog.T2syslogService;
 import thairice.mvc.t6org_data.T6org_data;
 
 /**
@@ -42,12 +45,14 @@ public class GrouthMonitorScheduleJob extends AbsScheduleJob implements ITask {
 	private int yearBeg = 2004;
 	private int yearEnd = 2019;
 
+	private static String jobName = GrouthMonitorScheduleJob.class.getSimpleName();
+	
 	private void initParams() {
 		List<Record> params = ConfMain.db()
 				.find("SELECT value_ FROM t1parameter where parm_type_id = '10000004' order by parm__id ");
 		if (CollectionUtils.isEmpty(params) || params.size() < 6) {
 			// 采用默认参数
-			log.error("initParams is null, use default params");
+			T2syslogService.warn(userId, userName, jobName, "initParams is null, use default params");
 			return;
 		}
 		threshold1 = Float.valueOf(params.get(0).getStr("value_").trim());
@@ -63,20 +68,20 @@ public class GrouthMonitorScheduleJob extends AbsScheduleJob implements ITask {
 		// todo 拼接查询条件, 还未确定, 确定后再做
 		String whereStr = sqlStr_ProcessStatus(EnumDataStatus.PDT_TYPE_Growth, EnumDataStatus.PROCESS_UN);
 		// sql 查询 为了参数有序，需要进行order by
-		return T12PreProcessInf.dao.find(String.format(" select * from %s where   %s and data_type =%s   limit 100 ",
-				T12PreProcessInf.tableName, whereStr, EnumDataStatus.PDT_TYPE_Growth.getIdStr()));
+		return T12PreProcessInf.dao.find(String.format(" select * from %s where   %s and data_type =%s   limit 10 ",
+				T12PreProcessInf.tableName, whereStr, EnumDataStatus.DATA_TYPE_NDVI_02.getIdStr()));
 	}
 
-	public List<Growth> mdlConvert(List<T12PreProcessInf> inputs) {
+	public List<Tuple2<Growth, Map<String, String>>>  mdlConvert(List<T12PreProcessInf> inputs) {
 		if (CollectionUtils.isEmpty(inputs)) {
-			throw new IllegalArgumentException(" mdlConvert inputs is null");
+			return Lists.newArrayList();
 		}
-		List<Growth> resList  = inputs.stream().map(preObj -> {
+		List<Tuple2<Growth, Map<String, String>>> resList  = inputs.stream().map(preObj -> {
 			Growth target = new Growth();
 			// 用该批数据的第一行id作为taskID
 			target.id = preObj.getId();
 			target.fileDate = GenerTimeStamp.pickDateStr(preObj.getData_collect_time());;
-			target.pathNdvi = (String)preObj.getFile_path()+(String)preObj.getFile_name();
+			target.pathNdvi = addFilePathName(preObj.getFile_path(), preObj.getFile_name());
 			target.imageLanduse = "D:\\grouth\\data\\Clip_N2_2017.tif";
 			target.shpfilePath = "D:\\Thailand_test\\landuse";
 			target.outPath = "D:\\Thailand_test\\grouth";
@@ -85,7 +90,22 @@ public class GrouthMonitorScheduleJob extends AbsScheduleJob implements ITask {
 			target.threshold2 = threshold2;
 			target.threshold3 = threshold3;
 			target.threshold4 = threshold4;
-			return target;
+			// 根据参数表 长势 开始年 到 结束年 预处理后的表中读collectime该月该日的
+			// 是NDVI_2类型 的所有文件(每一年会有一个)路径+文件名
+			Map<String, String> map = Maps.newHashMap();
+			String whereStr = sqlStr_ProcessStatus(EnumDataStatus.PDT_TYPE_Growth, EnumDataStatus.PROCESS_SUCCE);
+			List<T12PreProcessInf> listArg = T12PreProcessInf.dao.find(String.format(
+					" select * from %s where   %s and data_type =%s and date_format(data_collect_time, '%%Y%%m%%d') between %s and %s  limit 100 ",
+					T12PreProcessInf.tableName, whereStr, EnumDataStatus.DATA_TYPE_NDVI_02.getIdStr()
+					, GenerTimeStamp.pickYearMonthDay(yearBeg,preObj.getData_collect_time())
+					, GenerTimeStamp.pickYearMonthDay(yearEnd,preObj.getData_collect_time())));
+			if (Objects.isNull(listArg)) {
+				T2syslogService.error(userId, userName, jobName, "Objects.isNull(listArg)");
+			} else {
+				listArg.forEach(e->map.put(String.valueOf(e.getId())
+						, addFilePathName(e.getFile_path(),e.getFile_name())));
+			}
+			return TupleUtil.tuple(target, map);
 		}).collect(Collectors.toList());
 		
 		return resList;
@@ -100,6 +120,7 @@ public class GrouthMonitorScheduleJob extends AbsScheduleJob implements ITask {
 	public void run() {
 		// TODO Auto-generated method stub
 		boolean haveUndoData = false;
+		log.info(">>>>job begin");
 		while (haveUndoData) {
 			// 每批次读取一次参数配置
 			initParams();
@@ -111,23 +132,23 @@ public class GrouthMonitorScheduleJob extends AbsScheduleJob implements ITask {
 				return;
 			}
 			// 封装为rpc接口数据
-			List<Growth> rpcTodoDatas = mdlConvert(dbUndoDatas);
+			List<Tuple2<Growth, Map<String, String>>> rpcTodoDatas = mdlConvert(dbUndoDatas);
 
 			rpcTodoDatas.forEach(rpcData -> {
 				// 调用rpc处理程序
-				 EnumStatus rpcRes = growthMonitor(rpcData, null);
+				 EnumStatus rpcRes = growthMonitor(rpcData.first, rpcData.second);
 				 EnumDataStatus dbDataStatus = EnumDataStatus.PROCESS_SUCCE;
 				// 封装rpc结果数据，入库
 				 if (EnumStatus.Success != rpcRes) {
 					// todo 修改标志位为失败，等待下次任务继续执行，当失败超过3次则标志位终生失败
 					 dbDataStatus = EnumDataStatus.PROCESS_FAIL;
 				} 
-				 Record record = new Record().set(T12PreProcessInf.column_id, rpcData.id)
+				 Record record = new Record().set(T12PreProcessInf.column_id, rpcData.first.id)
 							.set(T12PreProcessInf.column_condition_st, dbDataStatus.getId());
 					ConfMain.db().update(T6org_data.tableName, record);
 			});
 		}
-		log.info("job all done");
+		log.info(">>> job end");
 	}
 
 	/*
--- src/thairice/rpcjob/LandYieldScheduleJob.java
+++ src/thairice/rpcjob/LandYieldScheduleJob.java
@@ -5,25 +5,29 @@
  */
 package thairice.rpcjob;
 
-import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.CollectionUtils;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.jfinal.log.Logger;
 import com.jfinal.plugin.activerecord.Record;
 import com.jfinal.plugin.cron4j.ITask;
 
 import RPCRice.Yield;
 import csuduc.platform.util.ComUtil;
+import csuduc.platform.util.FileUtils;
 import csuduc.platform.util.timeUtils.GenerTimeStamp;
 import csuduc.platform.util.tuple.Tuple2;
 import csuduc.platform.util.tuple.TupleUtil;
 import thairice.config.ConfMain;
 import thairice.constant.EnumStatus;
 import thairice.mvc.t12preprocessinf.T12PreProcessInf;
+import thairice.mvc.t2syslog.T2syslogService;
 
 /**
  * @author zw
@@ -32,84 +36,116 @@ import thairice.mvc.t12preprocessinf.T12PreProcessInf;
 public class LandYieldScheduleJob extends AbsScheduleJob implements ITask {
 
 	private static Logger log = Logger.getLogger(LandYieldScheduleJob.class);
-	
-	public List<T12PreProcessInf> loadDataFromDb(){
-		
+
+	private static String jobName = GrouthMonitorScheduleJob.class.getSimpleName();
+
+	private static String csvFilePath = "D:\\Thailand_test\\statistic\\";
+	public List<T12PreProcessInf> loadDataFromDb() {
+
 		String whereStr = sqlStr_ProcessStatus(EnumDataStatus.PDT_TYPE_Yield, EnumDataStatus.PROCESS_UN);
 		// sql 查询 为了参数有序，需要进行order by
-		return T12PreProcessInf.dao.find( String.format(" select * from %s where   %s and data_type =%s   limit 100 "
-				, T12PreProcessInf.tableName
-				, whereStr
-				, EnumDataStatus.PDT_TYPE_Yield.getIdStr()) );
+		return T12PreProcessInf.dao.find(String.format(" select * from %s where   %s and data_type =%s   limit 10 ",
+				T12PreProcessInf.tableName, whereStr, EnumDataStatus.DATA_TYPE_NDVI_02.getIdStr()));
 	}
-	
-	public List<Yield> mdlConvert(List<T12PreProcessInf> inputs ){
+
+	public List<Tuple2<Yield, Map<String, String>>> mdlConvert(List<T12PreProcessInf> inputs) {
 		if (CollectionUtils.isEmpty(inputs)) {
 			return Lists.newArrayList();
 		}
-		
-		List<Yield> resList  = inputs.stream().map(preObj -> {
+		int yearBeg = 2000;
+		// 前一年
+		int yearEnd = GenerTimeStamp.fetchYearByStep(-1);
+		List<Tuple2<Yield, Map<String, String>>> resList = inputs.stream().map(preObj -> {
 			Yield target = new Yield();
 			// 用该批数据的第一行id作为taskID
 			target.id = inputs.get(0).getId();
 			target.fileDate = GenerTimeStamp.pickDateStr(preObj.getData_collect_time());
-			target.pathNdvi = (String)preObj.getFile_path()+(String)preObj.getFile_name();
-			target.outCode = "72";
-			target.pathStatistics = "D:\\yield\\yield4\\statistic\\Suphanburd.csv";
+			target.pathNdvi = addFilePathName(preObj.getFile_path(), preObj.getFile_name());
+			// target.outCode = "72";
+			// target.pathStatistics = "D:\\yield\\yield4\\statistic\\Suphanburd.csv";
 			target.imageLanduse = "D:\\Thailand_test\\landuse";
 			target.outPath = " E:\\\\thairiceproduct\\\\Yield";
 			target.shpfilePath = "D:\\\\Thailand_test\\\\shp";
 			target.pathGdalwarpS = "C:\\warmerda\\bld\\bin\\gdalwarp.exe";
-			return target;
+			// 所有年的该月该日的历史数据 放入map
+			Map<String, String> map = Maps.newHashMap();
+			String whereStr = sqlStr_ProcessStatus(EnumDataStatus.PDT_TYPE_Growth, EnumDataStatus.PROCESS_SUCCE);
+			List<T12PreProcessInf> listArg = T12PreProcessInf.dao.find(String.format(
+					" select * from %s where  %s and data_type =%s and date_format(data_collect_time, '%%Y%%m%%d') between %s and %s  limit 100 ",
+					T12PreProcessInf.tableName, whereStr, EnumDataStatus.PDT_TYPE_Growth.getIdStr(),
+					GenerTimeStamp.pickYearMonthDay(yearBeg, preObj.getData_collect_time()),
+					GenerTimeStamp.pickYearMonthDay(yearEnd, preObj.getData_collect_time())));
+			if (Objects.isNull(listArg)) {
+				T2syslogService.error(userId, userName, jobName, "Objects.isNull(listArg)");
+			} else {
+				listArg.forEach(
+						e -> map.put(String.valueOf(e.getId()), addFilePathName(e.getFile_path(), e.getFile_name())));
+			}
+			return TupleUtil.tuple(target, map);
 		}).collect(Collectors.toList());
 
 		return resList;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Runnable#run()
 	 */
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		boolean haveUndoData = false;
-		while(haveUndoData){
+		log.info(">>>>job begin");
+		while (haveUndoData) {
 			// 从数据库读取数据
-			List<T12PreProcessInf> dbUndoDatas =  loadDataFromDb();
+			List<T12PreProcessInf> dbUndoDatas = loadDataFromDb();
 			if (ComUtil.isEmptyList(dbUndoDatas)) {
 				log.info("no predata now");
 				haveUndoData = false;
-				return ;
+				return;
 			}
 			// 封装为rpc接口数据
-			List<Yield> rpcTodoDatas = mdlConvert(dbUndoDatas);
-			 
+			List<Tuple2<Yield, Map<String, String>>> rpcTodoDatas = mdlConvert(dbUndoDatas);
+
+			// 存在全国省code 的csv才会调用，故便利csv文件来获取code
+			List<String> csvFileNames = FileUtils.getFileNames(csvFilePath);
+			if (CollectionUtils.isEmpty(csvFileNames)) {
+				T2syslogService.error(userId, userName, jobName, "no csvFiles");
+				break;
+			}
 			rpcTodoDatas.forEach(rpcData -> {
-				// 调用rpc处理程序
-				 EnumStatus rpcRes = landYield(rpcData, null);
-				 EnumDataStatus dbDataStatus = EnumDataStatus.PROCESS_SUCCE;
-				// 封装rpc结果数据，入库
-				 if (EnumStatus.Success != rpcRes) {
-					// todo 修改标志位为失败，等待下次任务继续执行，当失败超过3次则标志位终生失败
-					 dbDataStatus = EnumDataStatus.PROCESS_FAIL;
-				} 
-				 Record record = new Record().set(T12PreProcessInf.column_id, rpcData.id)
+				csvFileNames.forEach(csvFileName ->{
+					String[] tokens = csvFileName.split(".");
+					rpcData.first.outCode = tokens[0];
+					rpcData.first.pathStatistics = csvFilePath + csvFileName;
+					// 调用rpc处理程序
+					EnumStatus rpcRes = landYield(rpcData.first, rpcData.second);
+					EnumDataStatus dbDataStatus = EnumDataStatus.PROCESS_SUCCE;
+					// 封装rpc结果数据，入库
+					if (EnumStatus.Success != rpcRes) {
+						// todo 修改标志位为失败，等待下次任务继续执行，当失败超过3次则标志位终生失败
+						dbDataStatus = EnumDataStatus.PROCESS_FAIL;
+					}
+					Record record = new Record().set(T12PreProcessInf.column_id, rpcData.first.id)
 							.set(T12PreProcessInf.column_estimate_st, dbDataStatus.getId());
 					ConfMain.db().update(T12PreProcessInf.tableName, record);
+				});
+				
 			});
 		}
-		log.info("job all done");
+		log.info("<<<<job end");
 	}
 
-	
-	
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.jfinal.plugin.cron4j.ITask#stop()
 	 */
 	@Override
 	public void stop() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
