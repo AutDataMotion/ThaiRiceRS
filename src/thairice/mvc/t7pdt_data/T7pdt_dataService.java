39139024eef370c296994d415738968ad1abe331
lyf-wxy <513315356@qq.com>
2018-09-03 09:33:58 +0800

    update t7

--- src/thairice/mvc/t7pdt_data/T7pdt_dataService.java
+++ src/thairice/mvc/t7pdt_data/T7pdt_dataService.java
@@ -22,17 +22,17 @@ public class T7pdt_dataService extends BaseService {
 	}
 	public List<T7pdt_data> SelectByZone_code(String zone_code,String productKind_code,String startTime,String endTime){
 		
-		List<T7pdt_data> mdl = T7pdt_data.dao.find("select year(collect_time) as Year from t7pdt_data where zone_code=? and type_ = ? and collect_time>=? and collect_time<=?", zone_code,productKind_code,startTime,endTime);
+		List<T7pdt_data> mdl = T7pdt_data.dao.find("select distinct year(collect_time) as Year from t7pdt_data where zone_code=? and type_ = ? and collect_time>=? and collect_time<=?", zone_code,productKind_code,startTime,endTime);
 		return mdl;
 	}
 	public List<T7pdt_data> SelectByZone_code_year(String zone_code,String productKind_code,String startTime,String endTime,String year){
 		
-		List<T7pdt_data> mdl = T7pdt_data.dao.find("select month(collect_time) as month from t7pdt_data where zone_code=? and type_ = ? and collect_time>=? and collect_time<=? and year(collect_time) = ?", zone_code,productKind_code,startTime,endTime,year);
+		List<T7pdt_data> mdl = T7pdt_data.dao.find("select distinct month(collect_time) as month from t7pdt_data where zone_code=? and type_ = ? and collect_time>=? and collect_time<=? and year(collect_time) = ?", zone_code,productKind_code,startTime,endTime,year);
 		return mdl;
 	}
 	public List<T7pdt_data> SelectByZone_code_year_month(String zone_code,String productKind_code,String startTime,String endTime,String year,String month){
 		
-		List<T7pdt_data> mdl = T7pdt_data.dao.find("select day(collect_time) as day from t7pdt_data where zone_code=? and type_ = ? and collect_time>=? and collect_time<=? and year(collect_time) = ? and month(collect_time) = ?", zone_code,productKind_code,startTime,endTime,year,month);
+		List<T7pdt_data> mdl = T7pdt_data.dao.find("select distinct day(collect_time) as day from t7pdt_data where zone_code=? and type_ = ? and collect_time>=? and collect_time<=? and year(collect_time) = ? and month(collect_time) = ?", zone_code,productKind_code,startTime,endTime,year,month);
 		return mdl;
 	}
 }
